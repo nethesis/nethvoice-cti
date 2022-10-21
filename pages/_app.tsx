7444e6ccf4c918fcc00f0034767a2c391ad4f12b
Antonio <36625268+tonyco97@users.noreply.github.com>
2022-10-21 16:53:30 +0200

    Added Login page with authentication (#11)

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,12 +5,16 @@ import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import { Provider } from 'react-redux'
 import { store } from '../store'
+import Script from 'next/script'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <Provider store={store}>
-      <Component {...pageProps} />
-    </Provider>
+    <>
+      <Provider store={store}>
+        <Component {...pageProps} />
+      </Provider>
+      <Script src={`config/config.${process.env.NODE_ENV}.js`}></Script>
+    </>
   )
 }
 
