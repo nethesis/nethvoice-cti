3e6c045ddd258856c3df13cd96048314f88efacd
Antonio <36625268+tonyco97@users.noreply.github.com>
2022-10-26 12:12:38 +0200

    Changed loader and alert inside login (#15)

---
+++ stories/InlineNotification.stories.tsx
@@ -0,0 +1,42 @@
+import { InlineNotification, InlineNotifcationProps } from '../components/common'
+import { Meta, Story } from '@storybook/react'
+
+const meta = {
+  title: 'Components/InlineNotification',
+  component: InlineNotification,
+  argTypes: {
+  },
+  parameters: {
+    controls: { expanded: true },
+  },
+}
+
+export default meta as Meta
+
+const Template: Story<InlineNotifcationProps> = (args) => (
+  <InlineNotification {...args}>{args.children}</InlineNotification>
+)
+
+export const Information = Template.bind({})
+Information.args = {
+  children: 'Information',
+  type: 'info',
+}
+
+export const Warning = Template.bind({})
+Warning.args = {
+  children: 'Warning',
+  type: 'warning',
+}
+
+export const Error = Template.bind({})
+Error.args = {
+  children: 'Error',
+  type: 'error',
+}
+
+export const Success = Template.bind({})
+Success.args = {
+  children: 'Success',
+  type: 'success',
+}
