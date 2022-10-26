5a3f0ee5df7f83210cf7097b0555228b023758db
Sebastian <sebastian.besel@nethesis.it>
2022-10-26 14:36:03 +0200

    Add missing pages references (#17)

---
+++ pages/operators.tsx
@@ -0,0 +1,14 @@
+// Copyright (C) 2022 Nethesis S.r.l.
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
+import type { NextPage } from 'next'
+
+const Operators: NextPage = () => {
+
+  return (
+    <>
+    </>
+  )
+}
+
+export default Operators
