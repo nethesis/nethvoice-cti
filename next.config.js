cfa46ca015812f3333cea0c83b8bb76f25130832
Sebastian <sebastian.besel@nethesis.it>
2022-11-23 18:00:05 +0100

    Remove Janus use phone-island instead

--- next.config.js
+++ next.config.js
@@ -3,20 +3,7 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  output: 'standalone',
-  webpack: (config, { webpack }) => {
-    // Check the janus configuration for modules here: https://janus.conf.meetecho.com/docs/js-modules.html
-    config.plugins.push(new webpack.ProvidePlugin({ adapter: ['webrtc-adapter', 'default'] }))
-    config.module.rules.push({
-      test: require.resolve('janus-gateway'),
-      loader: 'exports-loader',
-      options: {
-        exports: 'Janus',
-      },
-    })
-    // Return config
-    return config
-  }
+  output: 'standalone'
 }
 
 module.exports = nextConfig
