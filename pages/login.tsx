eebe67923b97ef3f54db9864824134b6e34043d9
tonyco97 <tonyco97.tc@gmail.com>
2023-03-06 10:44:30 +0100

    Fixed login form padding for 2xl display

--- pages/login.tsx
+++ pages/login.tsx
@@ -87,7 +87,7 @@ export default function Login() {
   return (
     <>
       <div className='flex min-h-full'>
-        <div className='flex flex-1 flex-col justify-center py-12 px-4 sm:px-6 lg:flex-none lg:px-20 xl:px-24'>
+        <div className='flex flex-1 flex-col justify-center py-12 px-4 sm:px-6 lg:flex-none lg:px-20 xl:px-24 2xl:px-32'>
           <div className='mx-auto w-full max-w-sm lg:w-96'>
             <div className='flex flex-col items-center justify-center'>
               {' '}
