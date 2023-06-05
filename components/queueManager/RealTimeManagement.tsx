04ddffc3370bf4a88fd1ee35ca3a004d46b604bd
tonyco97 <tonyco97.tc@gmail.com>
2023-06-05 11:52:00 +0200

    Added filter inside realtime tab

--- components/queueManager/RealTimeManagement.tsx
+++ components/queueManager/RealTimeManagement.tsx
@@ -1,25 +1,20 @@
 // Copyright (C) 2023 Nethesis S.r.l.
 // SPDX-License-Identifier: AGPL-3.0-or-later
 
-import { FC, ComponentProps, useState, useEffect, Fragment } from 'react'
+import { FC, ComponentProps, useState, useEffect, useMemo } from 'react'
 import { useTranslation } from 'react-i18next'
-import { Button } from '../common'
-import { useSelector } from 'react-redux'
-import { RootState, store } from '../../store'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-
+import { debounce } from 'lodash'
 import {
   faChevronDown,
   faChevronUp,
-  faCheck,
-  faPhone,
   faUserCheck,
   faUserClock,
   faUserXmark,
   faHeadset,
-  faPause,
-  faDownLeftAndUpRightToCenter,
 } from '@fortawesome/free-solid-svg-icons'
+import { RealTimeOperatorsFilter } from './RealTimeOperatorsFilter'
+import { RealTimeQueuesFilter } from './RealTimeQueuesFilter'
 
 export interface RealTimeManagementProps extends ComponentProps<'div'> {}
 
@@ -29,10 +24,71 @@ function classNames(...classes: any) {
 
 export const RealTimeManagement: FC<RealTimeManagementProps> = ({ className }): JSX.Element => {
   const { t } = useTranslation()
-  const [expanded, setExpanded] = useState(false)
+  const [queuesStatisticsExpanded, setQueuesStatisticsExpanded] = useState(false)
+  const [operatorsStatisticsExpanded, setOperatorsStatisticsExpanded] = useState(false)
+  const [pageNum, setPageNum]: any = useState(1)
 
-  const toggleExpandQueue = () => {
-    setExpanded(!expanded)
+  const toggleExpandQueuesStatistics = () => {
+    setQueuesStatisticsExpanded(!queuesStatisticsExpanded)
+  }
+
+  const toggleExpandOperatorsStatistics = () => {
+    setOperatorsStatisticsExpanded(!operatorsStatisticsExpanded)
+  }
+
+  const [textFilter, setTextFilter]: any = useState('')
+
+  const updateTextFilter = (newTextFilter: string) => {
+    setTextFilter(newTextFilter)
+    setPageNum(1)
+  }
+
+  const debouncedUpdateTextFilterQueuesStatistics = useMemo(
+    () => debounce(updateTextFilter, 400),
+    [],
+  )
+
+  const debouncedUpdateTextFilterOperatorsStatistics = useMemo(
+    () => debounce(updateTextFilter, 400),
+    [],
+  )
+
+  // stop invocation of debounced function after unmounting
+  useEffect(() => {
+    return () => {
+      debouncedUpdateTextFilterQueuesStatistics.cancel()
+    }
+  }, [debouncedUpdateTextFilterQueuesStatistics])
+
+  // stop invocation of debounced function after unmounting
+  useEffect(() => {
+    return () => {
+      debouncedUpdateTextFilterOperatorsStatistics.cancel()
+    }
+  }, [debouncedUpdateTextFilterOperatorsStatistics])
+
+  const [outcomeFilterQueues, setOutcomeFilterQueues]: any = useState('')
+  const updateOutcomeFilterQueues = (newOutcomeFilter: string) => {
+    setOutcomeFilterQueues(newOutcomeFilter)
+    setPageNum(1)
+  }
+
+  const [queuesFilterQueues, setQueuesFilterQueues]: any = useState([])
+  const updateQueuesFilterQueues = (newQueuesFilter: string[]) => {
+    setQueuesFilterQueues(newQueuesFilter)
+    setPageNum(1)
+  }
+
+  const [outcomeFilterOperators, setOutcomeFilterOperators]: any = useState('')
+  const updateOutcomeFilterOperators = (newOutcomeFilter: string) => {
+    setOutcomeFilterQueues(newOutcomeFilter)
+    setPageNum(1)
+  }
+
+  const [queuesFilterOperators, setQueuesFilterOperators]: any = useState([])
+  const updateQueuesFilterOperators = (newQueuesFilter: string[]) => {
+    setQueuesFilterQueues(newQueuesFilter)
+    setPageNum(1)
   }
 
   return (
@@ -49,7 +105,6 @@ export const RealTimeManagement: FC<RealTimeManagementProps> = ({ className }): 
                     icon={faUserCheck}
                     className='h-6 w-6 cursor-pointer text-emerald-600 dark:text-emerald-600'
                     aria-hidden='true'
-                    // onClick={() => toggleExpandQueue(queue)}
                   />
                 </div>
                 <div className='flex justify-center'>
@@ -73,7 +128,6 @@ export const RealTimeManagement: FC<RealTimeManagementProps> = ({ className }): 
                     icon={faUserClock}
                     className='h-6 w-6 cursor-pointer text-emerald-600 dark:text-emerald-600'
                     aria-hidden='true'
-                    // onClick={() => toggleExpandQueue(queue)}
                   />
                 </div>
                 <div className='flex justify-center'>
@@ -97,7 +151,6 @@ export const RealTimeManagement: FC<RealTimeManagementProps> = ({ className }): 
                     icon={faHeadset}
                     className='h-6 w-6 cursor-pointer text-emerald-600 dark:text-emerald-600'
                     aria-hidden='true'
-                    // onClick={() => toggleExpandQueue(queue)}
                   />
                 </div>
                 <div className='flex justify-center'>
@@ -121,7 +174,6 @@ export const RealTimeManagement: FC<RealTimeManagementProps> = ({ className }): 
                     icon={faHeadset}
                     className='h-6 w-6 cursor-pointer text-emerald-600 dark:text-emerald-600'
                     aria-hidden='true'
-                    // onClick={() => toggleExpandQueue(queue)}
                   />
                 </div>
                 <div className='flex justify-center'>
@@ -145,7 +197,6 @@ export const RealTimeManagement: FC<RealTimeManagementProps> = ({ className }): 
                     icon={faUserXmark}
                     className='h-6 w-6 cursor-pointer text-emerald-600 dark:text-emerald-600'
                     aria-hidden='true'
-                    // onClick={() => toggleExpandQueue(queue)}
                   />
                 </div>
                 <div className='flex justify-center'>
@@ -169,7 +220,6 @@ export const RealTimeManagement: FC<RealTimeManagementProps> = ({ className }): 
                     icon={faHeadset}
                     className='h-6 w-6 cursor-pointer text-emerald-600 dark:text-emerald-600'
                     aria-hidden='true'
-                    // onClick={() => toggleExpandQueue(queue)}
                   />
                 </div>
                 <div className='flex justify-center'>
@@ -193,7 +243,6 @@ export const RealTimeManagement: FC<RealTimeManagementProps> = ({ className }): 
                     icon={faHeadset}
                     className='h-6 w-6 cursor-pointer text-emerald-600 dark:text-emerald-600'
                     aria-hidden='true'
-                    // onClick={() => toggleExpandQueue(queue)}
                   />
                 </div>
                 <div className='flex justify-center'>
@@ -217,7 +266,6 @@ export const RealTimeManagement: FC<RealTimeManagementProps> = ({ className }): 
                     icon={faHeadset}
                     className='h-6 w-6 cursor-pointer text-emerald-600 dark:text-emerald-600'
                     aria-hidden='true'
-                    // onClick={() => toggleExpandQueue(queue)}
                   />
                 </div>
                 <div className='flex justify-center'>
@@ -241,7 +289,6 @@ export const RealTimeManagement: FC<RealTimeManagementProps> = ({ className }): 
                     icon={faHeadset}
                     className='h-6 w-6 cursor-pointer text-emerald-600 dark:text-emerald-600'
                     aria-hidden='true'
-                    // onClick={() => toggleExpandQueue(queue)}
                   />
                 </div>
                 <div className='flex justify-center'>
@@ -260,7 +307,7 @@ export const RealTimeManagement: FC<RealTimeManagementProps> = ({ className }): 
       </div>
 
       {/* Queues statistics*/}
-      <div className='py-4 relative mt-4'>
+      <div className='pt-8 relative mt-4'>
         <div className='flex items-center space-x-1'>
           <div className='flex-grow'>
             <h2 className='text-lg font-semibold text-gray-900 dark:text-gray-100'>
@@ -269,15 +316,58 @@ export const RealTimeManagement: FC<RealTimeManagementProps> = ({ className }): 
           </div>
           <div className='flex items-center justify-end h-6 w-6'>
             <FontAwesomeIcon
-              icon={expanded ? faChevronDown : faChevronUp}
+              icon={queuesStatisticsExpanded ? faChevronDown : faChevronUp}
               className='h-3.5 w-3.5 pl-2 py-2 cursor-pointer flex items-center'
               aria-hidden='true'
-              onClick={toggleExpandQueue}
+              onClick={toggleExpandQueuesStatistics}
             />
           </div>
         </div>
         {/* divider */}
         <div className='flex-grow border-b border-gray-300 mt-1'></div>
+
+        {queuesStatisticsExpanded && (
+          <>
+            <RealTimeQueuesFilter
+              updateTextFilter={debouncedUpdateTextFilterQueuesStatistics}
+              updateOutcomeFilter={updateOutcomeFilterQueues}
+              updateQueuesFilter={updateQueuesFilterQueues}
+              className='pt-6'
+            ></RealTimeQueuesFilter>
+          </>
+        )}
+      </div>
+
+      {/* Operators statistics*/}
+      <div className='py-8 relative'>
+        <div className='flex items-center space-x-1'>
+          <div className='flex-grow'>
+            <h2 className='text-lg font-semibold text-gray-900 dark:text-gray-100'>
+              {t('QueueManager.Operators statistics')}
+            </h2>
+          </div>
+          <div className='flex items-center justify-end h-6 w-6'>
+            <FontAwesomeIcon
+              icon={operatorsStatisticsExpanded ? faChevronDown : faChevronUp}
+              className='h-3.5 w-3.5 pl-2 py-2 cursor-pointer flex items-center'
+              aria-hidden='true'
+              onClick={toggleExpandOperatorsStatistics}
+            />
+          </div>
+        </div>
+        {/* divider */}
+        <div className='flex-grow border-b border-gray-300 mt-1'></div>
+
+        {operatorsStatisticsExpanded && (
+          <>
+            <RealTimeOperatorsFilter
+              updateTextFilter={debouncedUpdateTextFilterOperatorsStatistics}
+              updateOutcomeFilter={updateOutcomeFilterOperators}
+              updateQueuesFilter={updateQueuesFilterOperators}
+              className='pt-6'
+            ></RealTimeOperatorsFilter>
+          </>
+        )}
       </div>
     </>
   )
