0dcc9d8d11f94c7533783e096f970c111ea1ac20
tonyco97 <tonyco97.tc@gmail.com>
2023-06-05 15:18:33 +0200

    Fixed realtime queues filter

--- components/queueManager/RealTimeManagement.tsx
+++ components/queueManager/RealTimeManagement.tsx
@@ -4,6 +4,10 @@
 import { FC, ComponentProps, useState, useEffect, useMemo } from 'react'
 import { useTranslation } from 'react-i18next'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { Avatar, Button, Dropdown, EmptyState, IconSwitch, TextInput } from '../common'
+import { useSelector } from 'react-redux'
+import { RootState, store } from '../../store'
+import { Tooltip } from 'react-tooltip'
 import { debounce } from 'lodash'
 import {
   faChevronDown,
@@ -12,7 +16,17 @@ import {
   faUserClock,
   faUserXmark,
   faHeadset,
+  faStar as faStarSolid,
 } from '@fortawesome/free-solid-svg-icons'
+import { faStar as faStarLight } from '@nethesis/nethesis-light-svg-icons'
+import {
+  searchStringInQueue,
+  addQueueToFavorites,
+  removeQueueFromFavorites,
+  addQueueToExpanded,
+  removeQueueFromExpanded,
+} from '../../lib/queuesLib'
+
 import { RealTimeOperatorsFilter } from './RealTimeOperatorsFilter'
 import { RealTimeQueuesFilter } from './RealTimeQueuesFilter'
 
@@ -26,7 +40,22 @@ export const RealTimeManagement: FC<RealTimeManagementProps> = ({ className }): 
   const { t } = useTranslation()
   const [queuesStatisticsExpanded, setQueuesStatisticsExpanded] = useState(false)
   const [operatorsStatisticsExpanded, setOperatorsStatisticsExpanded] = useState(false)
+  const [queueExpanded, setQueueExpanded] = useState(false)
   const [pageNum, setPageNum]: any = useState(1)
+
+  const authStore = useSelector((state: RootState) => state.authentication)
+
+  const toggleFavoriteQueue = (queue: any) => {
+    const queueId = queue.queue
+    const isFavorite = !queue.favorite
+    store.dispatch.queues.setQueueFavorite(queueId, isFavorite)
+
+    if (isFavorite) {
+      addQueueToFavorites(queueId, authStore.username)
+    } else {
+      removeQueueFromFavorites(queueId, authStore.username)
+    }
+  }
 
   const toggleExpandQueuesStatistics = () => {
     setQueuesStatisticsExpanded(!queuesStatisticsExpanded)
@@ -91,6 +120,62 @@ export const RealTimeManagement: FC<RealTimeManagementProps> = ({ className }): 
     setPageNum(1)
   }
 
+  const [filteredQueues, setFilteredQueues]: any = useState({})
+
+  const { name, mainPresence, mainextension, avatar, profile } = useSelector(
+    (state: RootState) => state.user,
+  )
+
+  const [isApplyingFilters, setApplyingFilters]: any = useState(false)
+
+  const queuesStore = useSelector((state: RootState) => state.queues)
+
+  const getQueuesUserLoggedIn = () => {
+    return Object.values(queuesStore.queues)
+      .filter((queue: any) => {
+        return (
+          queue.members[mainextension].loggedIn && queue.members[mainextension].type !== 'static'
+        )
+      })
+      .map((queue: any) => queue.queue)
+  }
+
+  const applyFilters = () => {
+    setApplyingFilters(true)
+
+    // text filter
+    let filteredQueues = Object.values(queuesStore.queues).filter((queue) =>
+      searchStringInQueue(queue, textFilter),
+    )
+
+    // sort queues
+    // filteredQueues.sort(sortByProperty('name'))
+    // filteredQueues.sort(sortByProperty('queue'))
+    // filteredQueues.sort(sortByFavorite)
+
+    setFilteredQueues(filteredQueues)
+    setApplyingFilters(false)
+  }
+
+  // filtered queues
+  useEffect(() => {
+    applyFilters()
+  }, [queuesStore.queues])
+
+  const toggleExpandQueue = (queue: any) => {
+    const queueId = queue.queue
+    const isExpanded = !queue.expanded
+    store.dispatch.queues.setQueueExpanded(queueId, isExpanded)
+
+    if (isExpanded) {
+      addQueueToExpanded(queueId, authStore.username)
+    } else {
+      removeQueueFromExpanded(queueId, authStore.username)
+    }
+    applyFilters()
+  }
+
+  
   return (
     <>
       {/* Dashboard queue active section */}
@@ -334,6 +419,78 @@ export const RealTimeManagement: FC<RealTimeManagementProps> = ({ className }): 
               updateQueuesFilter={updateQueuesFilterQueues}
               className='pt-6'
             ></RealTimeQueuesFilter>
+            <ul role='list' className='grid grid-cols-1 gap-6 xl:grid-cols-2 3xl:grid-cols-3'>
+              {/* skeleton */}
+              {/* {(!queuesStore.isLoaded || isApplyingFilters) &&
+                Array.from(Array(3)).map((e, i) => (
+                  <li
+                    key={i}
+                    className='col-span-1 rounded-md divide-y shadow divide-gray-200 bg-white dark:divide-gray-700 dark:bg-gray-900'
+                  >
+                    <div className='px-5 py-4'>
+                      {Array.from(Array(3)).map((e, j) => (
+                        <div key={j} className='space-y-4 mb-4'>
+                          <div className='animate-pulse h-5 rounded bg-gray-300 dark:bg-gray-600'></div>
+                          <div className='animate-pulse h-5 rounded max-w-[75%] bg-gray-300 dark:bg-gray-600'></div>
+                        </div>
+                      ))}
+                    </div>
+                  </li>
+                ))} */}
+              {/* queues */}
+              {queuesStore.isLoaded &&
+                Object.keys(filteredQueues).map((key) => {
+                  const queue = filteredQueues[key]
+                  return (
+                    <div key={queue.queue}>
+                      <li className='col-span-1 rounded-md divide-y shadow divide-gray-200 bg-white dark:divide-gray-700 dark:bg-gray-900'>
+                        {/* card header */}
+                        <div className='flex flex-col pt-3 pb-5 px-5'>
+                          <div className='flex w-full items-center justify-between space-x-6'>
+                            <div className='flex-1 truncate'>
+                              <div className='flex items-center space-x-2 py-1 text-gray-700 dark:text-gray-200'>
+                                <h3 className='truncate text-lg leading-6 font-medium'>
+                                  {queue.name}
+                                </h3>
+                                <span>{queue.queue}</span>
+                                <IconSwitch
+                                  on={queue.favorite}
+                                  size='large'
+                                  onIcon={<FontAwesomeIcon icon={faStarSolid} />}
+                                  offIcon={<FontAwesomeIcon icon={faStarLight} />}
+                                  changed={() => toggleFavoriteQueue(queue)}
+                                  key={queue.queue}
+                                  className={`tooltip-favorite-${queue.queue}`}
+                                >
+                                  <span className='sr-only'>
+                                    {t('Queues.Toggle favorite queue')}
+                                  </span>
+                                </IconSwitch>
+                                <Tooltip
+                                  anchorSelect={`.tooltip-favorite-${queue.queue}`}
+                                  place='top'
+                                >
+                                  {queue.favorite
+                                    ? t('Common.Remove from favorites') || ''
+                                    : t('Common.Add to favorites') || ''}
+                                </Tooltip>
+                              </div>
+                            </div>
+                            <FontAwesomeIcon
+                              icon={queue.expanded ? faChevronUp : faChevronDown}
+                              className='h-3.5 w-3.5 pl-2 py-2 cursor-pointer'
+                              aria-hidden='true'
+                              onClick={() => toggleExpandQueue(queue)}
+                            />
+                          </div>
+                        </div>
+                        {/* card body */}
+                        {queue.expanded && <></>}
+                      </li>
+                    </div>
+                  )
+                })}
+            </ul>
           </>
         )}
       </div>
