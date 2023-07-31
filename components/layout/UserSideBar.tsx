68aa31910670c75fc5d07005bcfcdfd72cf74287
Sebastian <sebastian.besel@nethesis.it>
2023-07-31 17:11:37 +0200

    Add user sidebar tabs menu

---
+++ components/layout/UserSideBar.tsx
@@ -0,0 +1,107 @@
+// Copyright (C) 2023 Nethesis S.r.l.
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
+/* eslint-disable react-hooks/exhaustive-deps */
+import { FC, useCallback, useEffect, useState } from 'react'
+import { SpeedDial } from './SpeedDial'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBolt, faPhone, type IconDefinition } from '@nethesis/nethesis-solid-svg-icons'
+import { getJSONItem, setJSONItem } from '../../lib/storage'
+import { RootState, store } from '../../store'
+import { useSelector } from 'react-redux'
+import { RootModel } from '../../models'
+
+const activeStyles = {
+  width: '.1875rem',
+  height: '1.25rem',
+  right: '0',
+  top: '.375rem',
+  borderRadius: '0rem .375rem .375rem 0rem',
+  transform: 'rotate(180deg)',
+  position: 'absolute',
+} as React.CSSProperties
+
+export const UserSideBar: FC = () => {
+  const [tabs, setTabs] = useState<TabTypes[]>([
+    {
+      icon: faBolt,
+      name: 'speed_dial',
+      active: false,
+    },
+    {
+      icon: faPhone,
+      name: 'last_calls',
+      active: false,
+    },
+  ])
+
+  const username = useSelector((state: RootState) => state.user.username)
+  const [tabReady, setTabReady] = useState<boolean>(false)
+
+  const changeTab = useCallback(
+    (name: string, save: boolean = false): void => {
+      let minOnce = false
+      setTabs((state) =>
+        state.map((tab) => {
+          if (tab.name === name) {
+            tab.active = true
+            minOnce = true
+          } else {
+            tab.active = false
+          }
+          return tab
+        }),
+      )
+      // Save the new tab to localstorage
+      if (minOnce && save) {
+        const preferences = getJSONItem(`preferences-${username}`) || {}
+        preferences['userSideBarTab'] = name
+        setJSONItem(`preferences-${username}`, preferences)
+      }
+    },
+    [username],
+  )
+
+  useEffect(() => {
+    if (username) {
+      const preferences = getJSONItem(`preferences-${username}`) || {}
+      if (preferences.userSideBarTab) {
+        changeTab(preferences.userSideBarTab)
+      } else {
+        changeTab('speed_dial')
+      }
+      setTabReady(true)
+    }
+  }, [username])
+
+  return (
+    <>
+      {/* The tabs content */}
+      {tabReady && tabs.map((tab) => tab.active && tab.name === 'speed_dial' && <SpeedDial />)}
+      {/* The side menu */}
+      <div
+        style={{ width: '3.125rem' }}
+        className='border-gray-200 dark:border-gray-700 border-l bg-white dark:bg-gray-900 py-6 flex flex-col items-center gap-6'
+      >
+        {tabs.map((tab, i) => (
+          <div
+            key={i}
+            onClick={() => changeTab(tab.name, true)}
+            className={`${
+              tab.active ? 'bg-gray-100 dark:bg-gray-700' : ''
+            } hover:bg-gray-100 hover:dark:bg-gray-700 w-8 h-8 rounded flex justify-center items-center relative cursor-pointer`}
+          >
+            <FontAwesomeIcon className='text-gray-600 dark:text-white' size='lg' icon={tab.icon} />
+            {tab.active && <div style={activeStyles} className='bg-primary' />}
+          </div>
+        ))}
+      </div>
+    </>
+  )
+}
+
+type TabTypes = {
+  icon: IconDefinition
+  name: 'speed_dial' | 'last_calls'
+  active: boolean
+}
