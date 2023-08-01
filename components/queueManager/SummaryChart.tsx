9bfd6d4af6637a1f4c5dd4cdf0a40cda3ebe29c1
tonyco97 <tonyco97.tc@gmail.com>
2023-08-01 17:35:30 +0200

    Chart tooltips

--- components/queueManager/SummaryChart.tsx
+++ components/queueManager/SummaryChart.tsx
@@ -4,6 +4,7 @@
 import { FC, ComponentProps } from 'react'
 import { useTranslation } from 'react-i18next'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { Tooltip } from 'react-tooltip'
 
 import { faCircleInfo } from '@fortawesome/free-solid-svg-icons'
 
@@ -33,9 +34,12 @@ export const SummaryChart: FC<SummaryChartProps> = ({ className }): JSX.Element 
                   <div className='flex items-center'>
                     <FontAwesomeIcon
                       icon={faCircleInfo}
-                      className='h-5 w-5 pl-2 py-2 cursor-pointer flex items-center'
+                      className='h-5 w-5 pl-2 py-2 cursor-pointer flex items-center tooltip-total-calls'
                       aria-hidden='true'
                     />
+                    <Tooltip anchorSelect='.tooltip-total-calls' place='left'>
+                      {t('QueueManager.SummaryTotalCallChartDescription') || ''}
+                    </Tooltip>
                   </div>
                 </div>
               </div>
@@ -51,9 +55,12 @@ export const SummaryChart: FC<SummaryChartProps> = ({ className }): JSX.Element 
                   <div className='flex items-center'>
                     <FontAwesomeIcon
                       icon={faCircleInfo}
-                      className='h-5 w-5 pl-2 py-2 cursor-pointer flex items-center'
+                      className='h-5 w-5 pl-2 py-2 cursor-pointer flex items-center tooltip-answered-calls'
                       aria-hidden='true'
                     />
+                    <Tooltip anchorSelect='.tooltip-answered-calls' place='left'>
+                      {t('QueueManager.SummaryAnsweredCallChartDescription') || ''}
+                    </Tooltip>
                   </div>
                 </div>
               </div>
@@ -69,9 +76,12 @@ export const SummaryChart: FC<SummaryChartProps> = ({ className }): JSX.Element 
                   <div className='flex items-center'>
                     <FontAwesomeIcon
                       icon={faCircleInfo}
-                      className='h-5 w-5 pl-2 py-2 cursor-pointer flex items-center'
+                      className='h-5 w-5 pl-2 py-2 cursor-pointer flex items-center tooltip-answered-before-service'
                       aria-hidden='true'
                     />
+                    <Tooltip anchorSelect='.tooltip-answered-before-service' place='left'>
+                      {t('QueueManager.SummaryAnsweredBeforeServiceChartDescription') || ''}
+                    </Tooltip>
                   </div>
                 </div>
               </div>
@@ -87,9 +97,12 @@ export const SummaryChart: FC<SummaryChartProps> = ({ className }): JSX.Element 
                   <div className='flex items-center'>
                     <FontAwesomeIcon
                       icon={faCircleInfo}
-                      className='h-5 w-5 pl-2 py-2 cursor-pointer flex items-center'
+                      className='h-5 w-5 pl-2 py-2 cursor-pointer flex items-center tooltip-unanswered'
                       aria-hidden='true'
                     />
+                    <Tooltip anchorSelect='.tooltip-unanswered' place='left'>
+                      {t('QueueManager.SummaryUnansweredChartDescription') || ''}
+                    </Tooltip>
                   </div>
                 </div>
               </div>
@@ -105,9 +118,12 @@ export const SummaryChart: FC<SummaryChartProps> = ({ className }): JSX.Element 
                   <div className='flex items-center'>
                     <FontAwesomeIcon
                       icon={faCircleInfo}
-                      className='h-5 w-5 pl-2 py-2 cursor-pointer flex items-center'
+                      className='h-5 w-5 pl-2 py-2 cursor-pointer flex items-center tooltip-unanswered-for_reasons'
                       aria-hidden='true'
                     />
+                    <Tooltip anchorSelect='.tooltip-unanswered-for_reasons' place='left'>
+                      {t('QueueManager.SummaryUnansweredForReasonsChartDescription') || ''}
+                    </Tooltip>
                   </div>
                 </div>
               </div>
@@ -123,9 +139,12 @@ export const SummaryChart: FC<SummaryChartProps> = ({ className }): JSX.Element 
                   <div className='flex items-center'>
                     <FontAwesomeIcon
                       icon={faCircleInfo}
-                      className='h-5 w-5 pl-2 py-2 cursor-pointer flex items-center'
+                      className='h-5 w-5 pl-2 py-2 cursor-pointer flex items-center tooltip-callback'
                       aria-hidden='true'
                     />
+                    <Tooltip anchorSelect='.tooltip-callback' place='left'>
+                      {t('QueueManager.Callback time') || ''}
+                    </Tooltip>
                   </div>
                 </div>
               </div>
@@ -141,27 +160,33 @@ export const SummaryChart: FC<SummaryChartProps> = ({ className }): JSX.Element 
                   <div className='flex items-center'>
                     <FontAwesomeIcon
                       icon={faCircleInfo}
-                      className='h-5 w-5 pl-2 py-2 cursor-pointer flex items-center'
+                      className='h-5 w-5 pl-2 py-2 cursor-pointer flex items-center tooltip-invalid-calls'
                       aria-hidden='true'
                     />
+                    <Tooltip anchorSelect='.tooltip-invalid-calls' place='left'>
+                      {t('QueueManager.SummaryInvalidCallsChartDescription') || ''}
+                    </Tooltip>
                   </div>
                 </div>
               </div>
             </div>
 
-            {/* Waiting calls */}
+            {/* Waiting time */}
             <div>
               <div className='border-b rounded-lg shadow-md border-gray-200 dark:border-gray-700 bg-white dark:bg-gray-900 px-5 py-3 sm:mt-1 relative flex items-center'>
                 <div className='flex items-center justify-between w-full'>
                   <span className='text-sm font-medium leading-6 text-gray-700 dark:text-gray-100'>
-                    {t('QueueManager.Waiting calls')}
+                    {t('QueueManager.Waiting times')}
                   </span>
                   <div className='flex items-center'>
                     <FontAwesomeIcon
                       icon={faCircleInfo}
-                      className='h-5 w-5 pl-2 py-2 cursor-pointer flex items-center'
+                      className='h-5 w-5 pl-2 py-2 cursor-pointer flex items-center tooltip-waiting-times'
                       aria-hidden='true'
                     />
+                    <Tooltip anchorSelect='.tooltip-waiting-times' place='left'>
+                      {t('QueueManager.SummaryWaitingTimesChartDescription') || ''}
+                    </Tooltip>
                   </div>
                 </div>
               </div>
@@ -177,9 +202,12 @@ export const SummaryChart: FC<SummaryChartProps> = ({ className }): JSX.Element 
                   <div className='flex items-center'>
                     <FontAwesomeIcon
                       icon={faCircleInfo}
-                      className='h-5 w-5 pl-2 py-2 cursor-pointer flex items-center'
+                      className='h-5 w-5 pl-2 py-2 cursor-pointer flex items-center tooltip-calls-duration'
                       aria-hidden='true'
                     />
+                    <Tooltip anchorSelect='.tooltip-calls-duration' place='left'>
+                      {t('QueueManager.SummaryCallsDurationChartDescription') || ''}
+                    </Tooltip>
                   </div>
                 </div>
               </div>
