e4a341000989a3b9cad68d2259d5a4983e62ec62
tonyco97 <tonyco97.tc@gmail.com>
2023-09-14 18:00:36 +0200

    Fixed qrCode

--- components/settings/QRCode.tsx
+++ components/settings/QRCode.tsx
@@ -1,17 +1,8 @@
 // Copyright (C) 2023 Nethesis S.r.l.
 // SPDX-License-Identifier: AGPL-3.0-or-later
 
-import { useEffect, useRef, useState, ChangeEvent } from 'react'
-import QRCodeStyling, {
-  DrawType,
-  TypeNumber,
-  Mode,
-  ErrorCorrectionLevel,
-  DotType,
-  CornerSquareType,
-  CornerDotType,
-  Options,
-} from 'qr-code-styling'
+import { useEffect, useRef } from 'react'
+import qrcode from 'qrcode'
 
 import { faDownload } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
@@ -25,70 +16,39 @@ interface QRCodeTypes {
 }
 
 export default function QRCode({ data }: QRCodeTypes) {
-  const [options, setOptions] = useState<Options>({
-    width: 300,
-    height: 300,
-    type: 'svg' as DrawType,
-    data: data,
-    image: '/navbar_logo.svg',
-    margin: 0,
-    qrOptions: {
-      typeNumber: 0 as TypeNumber,
-      mode: 'Byte' as Mode,
-      errorCorrectionLevel: 'Q' as ErrorCorrectionLevel,
-    },
-    imageOptions: {
-      hideBackgroundDots: true,
-      imageSize: 0.4,
-      margin: 20,
-      crossOrigin: 'anonymous',
-    },
-    dotsOptions: {
-      color: '#222222',
-      type: 'rounded' as DotType,
-    },
-    backgroundOptions: {
-      color: 'white',
-    },
-    cornersSquareOptions: {
-      color: '#222222',
-      type: 'extra-rounded' as CornerSquareType,
-    },
-    cornersDotOptions: {
-      color: '#222222',
-      type: 'dot' as CornerDotType,
-    },
-  })
-  const [qrCode] = useState<QRCodeStyling>(new QRCodeStyling(options))
-  const ref = useRef<HTMLDivElement>(null)
+  const ref = useRef<HTMLCanvasElement | null>(null)
 
   useEffect(() => {
     if (ref.current) {
-      qrCode.append(ref.current)
+      qrcode.toCanvas(ref.current, data, function (error) {
+        if (error) console.error(error)
+      })
     }
-  }, [qrCode, ref])
-
-  useEffect(() => {
-    if (!qrCode) return
-    qrCode.update(options)
-  }, [qrCode, options])
-
-  useEffect(() => {
-    setOptions((options) => ({
-      ...options,
-      data: data,
-    }))
   }, [data])
 
   return (
     <div className='flex flex-col gap-5'>
-      <div ref={ref} />
+      <canvas ref={ref} />
       <div>
-        <Button variant='white' onClick={() => qrCode.download()}>
+        <Button variant='white' onClick={() => downloadQRCode(data)}>
           <span>{t('Common.Download')}</span>
           <FontAwesomeIcon icon={faDownload} className='ml-2 h-4 w-4' />
         </Button>
       </div>
     </div>
   )
+
+  function downloadQRCode(data: string) {
+    const canvas = ref.current
+    if (canvas) {
+      const link = document.createElement('a')
+      canvas.toBlob((blob) => {
+        if (blob) {
+          link.href = URL.createObjectURL(blob)
+          link.download = 'qrcode.png'
+          link.click()
+        }
+      })
+    }
+  }
 }
