6220e1068039fe37a2e310a8b910e66b5beb809a
tonyco97 <tonyco97.tc@gmail.com>
2023-12-01 16:45:44 +0100

    Fixed wrong time inside queue

--- components/history/CallsDate.tsx
+++ components/history/CallsDate.tsx
@@ -46,6 +46,18 @@ const getCallDistanceToNowTemplate = (callTime: any) => {
   }
 }
 
+const getCallDistanceToNowIsInQueue = (callTime: any) => {
+  const timeZone = Intl.DateTimeFormat().resolvedOptions().timeZone
+  const callDate = utcToZonedTime(new Date(callTime), timeZone)
+  const timeDistance = formatDistanceToNow(callDate, { addSuffix: true, includeSeconds: false })
+
+  if (timeDistance !== '') {
+    return customFormatDistance(callDate)
+  } else {
+    return t('Common.0 minutes ago')
+  }
+}
+
 const getCallDistanceToNowTemplateIsAnnouncement = (date: any) => {
   const dateParts = date?.date_creation.split('/')
   const timeParts = date?.time_creation.split(':')
@@ -96,7 +108,7 @@ export const CallsDate: FC<CallsDateProps> = ({ call, spaced, isInQueue, isInAnn
       ) : (
         <div className={`flex flex-col justify-center flex-shrink-0 ${spaced ? 'gap-1.5' : ''}`}>
           <div className='text-sm font-medium text-gray-600 dark:text-gray-100 leading-5	'>
-            {getCallDistanceToNowTemplate(call?.time * 1000)}
+            {getCallDistanceToNowIsInQueue(call?.time * 1000)}
           </div>
           <div className='text-sm text-gray-600 dark:text-gray-100 font-normal leading-5	'>
             ({formatDateLocIsDifferentTimezone(call?.time * 1000, 'PP')}{' '}
