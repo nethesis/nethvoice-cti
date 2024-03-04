c10f4cf07d0a5bf743a3aadbbcd1d92f439dbe8b
tonyco97 <tonyco97.tc@gmail.com>
2024-03-04 11:20:27 +0100

    User sidebar menu. Fixed missing update on local storage

--- components/layout/UserNavBar.tsx
+++ components/layout/UserNavBar.tsx
@@ -1,4 +1,4 @@
-// Copyright (C) 2023 Nethesis S.r.l.
+// Copyright (C) 2024 Nethesis S.r.l.
 // SPDX-License-Identifier: AGPL-3.0-or-later
 
 /* eslint-disable react-hooks/exhaustive-deps */
@@ -6,7 +6,7 @@ import { FC, useCallback, useEffect, useState } from 'react'
 import { SpeedDial } from './SpeedDial'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faBolt, faPhone, type IconDefinition } from '@fortawesome/free-solid-svg-icons'
-import { getJSONItem, setJSONItem } from '../../lib/storage'
+import { getJSONItem, loadPreference, setJSONItem } from '../../lib/storage'
 import { RootState, store } from '../../store'
 import { useSelector } from 'react-redux'
 import { UserLastCalls } from './UserLastCalls'
@@ -28,6 +28,8 @@ export const UserNavBar: FC = () => {
   const username = useSelector((state: RootState) => state.user.username)
   const [tabReady, setTabReady] = useState<boolean>(false)
 
+  const auth = useSelector((state: RootState) => state.authentication)
+
   const [tabs, setTabs] = useState<TabTypes[]>([
     {
       icon: faBolt,
@@ -43,49 +45,9 @@ export const UserNavBar: FC = () => {
     },
   ])
 
-  const changeTab = useCallback(
-    (name: string, save: boolean = false): void => {
-      let minOnce = false
-      setTabs((state) =>
-        state.map((tab) => {
-          if (tab.name === name) {
-            tab.active = true
-            minOnce = true
-          } else {
-            tab.active = false
-          }
-          return tab
-        }),
-      )
-      // Save the new tab to localstorage
-      if (minOnce && save) {
-        const preferences = getJSONItem(`preferences-${username}`) || {}
-        preferences['userSideBarTab'] = name
-        setJSONItem(`preferences-${username}`, preferences)
-      }
-    },
-    [username],
-  )
-
   const rightSideStatus: any = useSelector((state: RootState) => state.rightSideMenu)
 
   const [defaultTabSelected, setDefaultTabSelected] = useState('')
-
-  useEffect(() => {
-    if (username) {
-      const preferences = getJSONItem(`preferences-${username}`) || {}
-      if (preferences?.userSideBarTab) {
-        changeTab(preferences?.userSideBarTab)
-        //set default if user has not changed tab
-        setDefaultTabSelected(preferences?.userSideBarTab)
-      } else {
-        changeTab('speed_dial')
-        //set default if user has not changed tab
-        setDefaultTabSelected('speed_dial')
-      }
-      setTabReady(true)
-    }
-  }, [username])
 
   //On first render of page get value of tab from local storage
   useEffect(() => {
@@ -94,8 +56,29 @@ export const UserNavBar: FC = () => {
     }
   }, [rightSideStatus?.actualTab, defaultTabSelected])
 
+  const [firstClickOnTab, setFirstClickOnTab] = useState(false)
+
   const clickedTab = (tabName: any, save: boolean) => {
     let rightSideMenuOpened = rightSideStatus?.isSideMenuOpened
+    // avoid conflict with useEffect
+    if (!firstClickOnTab) {
+      setFirstClickOnTab(true)
+    }
+    setDefaultTabSelected(tabName)
+    const preferences = getJSONItem(`preferences-${username}`) || {}
+    preferences['userSideBarTab'] = tabName
+    setJSONItem(`preferences-${username}`, preferences)
+    setTabs((state) =>
+      state.map((tab) => {
+        if (tab?.name === tabName) {
+          tab.active = true
+        } else {
+          tab.active = false
+        }
+        return tab
+      }),
+    )
+
     // Check if selected tab is already active
     const isTabActive = tabs.find((tab) => tab.name === tabName)?.active
 
@@ -112,7 +95,6 @@ export const UserNavBar: FC = () => {
       store.dispatch.rightSideMenu.setShown(true)
       store.dispatch.rightSideMenu.setRightSideMenuOpened(true)
       saveTabStatusToStorage(true)
-      changeTab(tabName, save)
     }
   }
 
@@ -125,6 +107,51 @@ export const UserNavBar: FC = () => {
   const [firstRender, setFirstRender] = useState(true)
 
   const userPreferences = getJSONItem(`preferences-${username}`) || {}
+
+  const getTabValuesFromLocalStorage = (currentUsername: string) => {
+    const rightSideMenuTabSelectedLocalStorage = loadPreference('userSideBarTab', currentUsername)
+    return { rightSideMenuTabSelectedLocalStorage }
+  }
+
+  //Get selected user side menu selected value from the local storage
+  useEffect(() => {
+    const localStorageTabValues = getTabValuesFromLocalStorage(auth.username)
+    if (
+      localStorageTabValues?.rightSideMenuTabSelectedLocalStorage &&
+      localStorageTabValues?.rightSideMenuTabSelectedLocalStorage !== ''
+    ) {
+      setDefaultTabSelected(localStorageTabValues?.rightSideMenuTabSelectedLocalStorage)
+      if (!firstClickOnTab) {
+        setTabs((state) =>
+          state.map((tab) => {
+            if (tab?.name === localStorageTabValues?.rightSideMenuTabSelectedLocalStorage) {
+              tab.active = true
+            } else {
+              tab.active = false
+            }
+            return tab
+          }),
+        )
+      }
+    } else {
+      setDefaultTabSelected('speed_dial')
+      if (!firstClickOnTab) {
+        setTabs((state) =>
+          state.map((tab) => {
+            if (tab?.name === 'speed_dial') {
+              tab.active = true
+            } else {
+              tab.active = false
+            }
+            return tab
+          }),
+        )
+      }
+    }
+
+    setTabReady(true)
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
 
   //Get the selected filter from the local storage
   useEffect(() => {
@@ -146,7 +173,7 @@ export const UserNavBar: FC = () => {
       {/* The tabs content */}
       {tabReady &&
         tabs.map((tab, i) => {
-          if (tab.active && tab.name === 'speed_dial') {
+          if (tab?.active && tab?.name === 'speed_dial') {
             return <SpeedDial key={i} />
           } else if (tab.active && tab.name === 'last_calls') {
             return <UserLastCalls key={i} />
@@ -160,17 +187,17 @@ export const UserNavBar: FC = () => {
         {tabs.map((tab, i) => (
           <div
             key={i}
-            onClick={() => clickedTab(tab.name, true)}
+            onClick={() => clickedTab(tab?.name, true)}
             className={`${
               tab.active
                 ? 'bg-gray-100 dark:bg-gray-700 text-gray-900 dark:text-gray-50'
                 : 'text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-50'
             } hover:bg-gray-100 hover:dark:bg-gray-700 w-8 h-8 rounded flex justify-center items-center relative cursor-pointer`}
             data-tooltip-id={'tooltip-side-menu'}
-            data-tooltip-content={tab.label}
+            data-tooltip-content={tab?.label}
           >
             <FontAwesomeIcon size='lg' icon={tab.icon} />
-            {tab.active && <div style={activeStyles} className='bg-primary dark:bg-primaryDark' />}
+            {tab?.active && <div style={activeStyles} className='bg-primary dark:bg-primaryDark' />}
           </div>
         ))}
       </div>
