1cd441983b90562bfc7c521b666376593ffdd476
Tommaso Ascani <tommaso.ascani@gmail.com>
2025-03-07 11:10:34 +0100

    feat: add Recordings and VoicemailInbox components, update translations and links in VoiceMailContent

---
+++ components/history/voicemail inbox/VoicemailInbox.tsx
@@ -0,0 +1,15 @@
+// Copyright (C) 2025 Nethesis S.r.l.
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
+import { FC, ComponentProps } from 'react'
+
+export interface VoicemailInboxProps extends ComponentProps<'div'> {}
+
+export const VoicemailInbox: FC<VoicemailInboxProps> = ({ className }): JSX.Element => {
+    return (
+    <>
+    </>
+  )
+}
+
+VoicemailInbox.displayName = 'Voicemail Inbox'
