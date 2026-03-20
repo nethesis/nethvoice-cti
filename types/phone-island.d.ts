6197dfc797b3e64a1416bbc4b38fd47e3018d39c
Tommaso Ascani <tommaso.ascani@gmail.com>
2026-03-20 17:06:15 +0100

    feat: enhance PhoneIsland configuration and add extension retrieval functionality

--- types/phone-island.d.ts
+++ types/phone-island.d.ts
@@ -2,6 +2,7 @@ declare module '@nethesis/phone-island' {
   export interface PhoneIslandProps {
     dataConfig: string;
     uaType: 'desktop' | 'mobile';
+    preferredSummaryExtensionType?: 'webrtc';
   }
 
   export const PhoneIsland: React.FC<PhoneIslandProps>;
